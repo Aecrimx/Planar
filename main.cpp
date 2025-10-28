63e8de7bf2fc9acc0e3de86c7f4b781b6361a45e
Aecrimx <mircea.alexandru.popa@gmail.com>
2025-10-28 18:24:58 +0200

    rescriere totala de main, insa input-ul e broken(nu se updateaza Render(). Adaugat baza de citire/salvare config file json

--- main.cpp
+++ main.cpp
@@ -1,87 +1,240 @@
-#include <ftxui/component/component.hpp>       // for Slider, Renderer, Vertical
-#include <ftxui/component/component_base.hpp>  // for ComponentBase
-#include <ftxui/component/screen_interactive.hpp>  // for ScreenInteractive
-#include <ftxui/dom/elements.hpp>  // for separator, operator|, Element, size, text, vbox, xflex, bgcolor, hbox, GREATER_THAN, WIDTH, border, HEIGHT, LESS_THAN
-#include <ftxui/screen/color.hpp>  // for Color
-#include <cpr/cpr.h>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
 #include <nlohmann/json.hpp>
+#include <ftxui/screen/color.hpp>
+
 #include <string>
 #include <vector>
+#include <memory>
+#include <sstream>
+#include <chrono>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 
 using namespace ftxui;
 
-class WindowBox {
+namespace fs = std::filesystem;
+using json = nlohmann::json;
+
+class Config {
 private:
-    std::string title_;
-    std::string content_; // PT MAI TARZIU ADAUGAT CONTINUTUL WINDOWULUI
+    std::string filename = "profile_config.json";
+
 public:
-    WindowBox(const std::string& title,const std::string& content) : title_(title), content_(content) {} //kinda broken
+    int menu_selection = 0;
+    int bar_selection = 0;
 
-
-    Element Render() const {
-        Element title = text(title_) | italic; // aici nu merge | hcenter :(
-        //TO-DO maybe find a hackish way sa fac titlul aliniat pe centru?
-        return window(title, paragraph(content_)); // AICI CONTENTUL
+    Config() {
+        load();
     }
-    //TO- DO .inner = funcContent();
-    void SetTitle(const std::string& new_title) {title_ = new_title;}
-    void SetContent(const std::string& new_content) {content_ = new_content;} //same stuff
+
+    // Load or create the config file
+    void load() {
+        if (fs::exists(filename)) {
+            std::ifstream file(filename);
+            if (!file.is_open()) {
+                std::cerr << "Could not open existing config file. Using defaults.\n";
+                return;
+            }
+
+            json j;
+            try {
+                file >> j;
+                menu_selection = j.value("menu_selection", 0);
+                bar_selection = j.value("bar_selection", 0);
+            } catch (const json::parse_error& e) {
+                std::cerr << "json : Failed to parse config file: " << e.what() << "\n";
+            }
+        } else {
+            save();
+        }
+    }
+
+    void save() {
+        json j = {
+            {"menu_selection", menu_selection},
+            {"bar_selection", bar_selection}
+            //for now
+        };
+        std::ofstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Error: Could not create/save config file.\n";
+            return;
+        }
+        file << std::setw(4) << j << "\n";
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Config& cfg) {
+        return os << "Menu selection: " << cfg.menu_selection << '\n'
+                  << "Bar selection: " << cfg.bar_selection << '\n';
+    }
 };
 
-
-class BorderRenderer {
+class MenuContentBase : public ComponentBase{
 private:
+    std::vector<std::string> main_entries = {
+        "Calendar",
+        "Journal",
+        "Timer"
+        //To be added
+    };
+    std::vector<std::string> bar_entries = {
+        "Weather",
+        "Checklist"
+        //To be added
+    };
+    std::string menu_title;
+
+    int main_menu_selected = 0;
+    int bar_menu_selected = 0;
+    bool main_menu_active = false;
+    bool bar_menu_active = false;
+
+    Component layout;
+
+public:
+    MenuContentBase() {
+        layout = Container::Vertical({
+        Checkbox("Main", &main_menu_active),
+        Radiobox(&main_entries, &main_menu_selected) | Maybe(&main_menu_active),
+        Checkbox("Bar", &bar_menu_active),
+        Radiobox(&bar_entries, &bar_menu_selected) | Maybe(&bar_menu_active),
+    });
+        Add(layout); // din ComponentBase; il transforma in child component
+    }
+    MenuContentBase(int main_menu_selected_, int bar_menu_selected_) // Will load from profile
+    : main_menu_selected(main_menu_selected_),
+    bar_menu_selected(bar_menu_selected_) {
+        layout = Container::Vertical({
+        Checkbox("Main", &main_menu_active),
+        Radiobox(&main_entries, &main_menu_selected) | Maybe(&main_menu_active),
+        Checkbox("Bar", &bar_menu_active),
+        Radiobox(&bar_entries, &bar_menu_selected) | Maybe(&bar_menu_active),
+    });
+
+        Add(layout);
+    }
+
+    //GETTER pt Element de render pasat la clasa care renderuieste
+    Element Render() {
+        return window(text(menu_title) | bold,
+            layout->Render() | borderRounded
+            );
+    }
+    int GetMainSelected() const { return main_menu_selected; }
+    int GetBarSelected() const { return bar_menu_selected; }
+
+
+    ~MenuContentBase() override{}
+};
+ftxui::Component MenuContent(int& main_menu_selected, int& bar_menu_selected) {
+    return ftxui::Make<MenuContentBase>(main_menu_selected, bar_menu_selected);
+} // CONVERSIE
+
+
+class App {
+private:
+    Config current_config;
+    Component app_layout; //layout-ul final de display
     ScreenInteractive& screen;
-    std::vector<WindowBox> windows_;
+
+    //AIDI COMPONENTELE DIVERSE
+    Component menu;
+    std::vector<Component> main_content_components; // vom baga in asta in aceiasi ordine componentelle ca in menu
+    std::vector<Component> bar_content_components;
+
     int width = screen.dimx();
     int height = screen.dimy();
-public:
-    BorderRenderer(ScreenInteractive& screen_ref) : screen(screen_ref) {
 
-        WindowBox MenuBox = WindowBox("Menu", "Content Menu");
-        windows_.push_back(MenuBox);
+    Component root_component; //entire layout
 
-        WindowBox MainBox = WindowBox("Main", "Content Main");
-        windows_.push_back(MainBox);
-
-        WindowBox BarBox = WindowBox("Bar", "Content Bar");
-        windows_.push_back(BarBox);
+    Component GetCurrentMain() {
+        return main_content_components.at(current_config.menu_selection);
+    }
+    Component GetCurrentBarComponent() {
+        return bar_content_components.at(current_config.bar_selection);
     }
 
-    Element Render() const {
-        Element top = hbox({
-            windows_[0].Render() | flex,
-            windows_[1].Render() | flex,
-        }) | flex;
-
-        Element bottom = windows_[2].Render() | flex;
-
-        return vbox({
-            top,
-            bottom,
-        }) | border;
-    }
-};
-
-
-
-class BorderScreen {
-private:
-    ScreenInteractive screen;
 public:
-    BorderScreen() : screen(ScreenInteractive::Fullscreen()) {}
+    App(ScreenInteractive& screen_) : screen(screen_) {}
+    App(ScreenInteractive& screen_, Config& current_config_) : screen(screen_),
+    current_config(current_config_) {
+        menu = MenuContent(current_config.menu_selection, current_config.bar_selection);
+
+        //si celelalte elemente
+
+
+        // main_content_components = {
+        //     //chestii
+        // };
+        // bar_content_components = {
+        //     //chestii
+        // };
+
+        main_content_components = {
+            Renderer([] { return text("Calendar Placeholder") | border; }),
+            Renderer([] { return text("Journal Placeholder") | border; }),
+            Renderer([] { return text("Timer Placeholder") | border; }),
+        };
+
+        bar_content_components = {
+            Renderer([] { return text("Weather Placeholder") | border; }),
+            Renderer([] { return text("Checklist Placeholder") | border; }),
+        };
+
+        auto content_container = Container::Vertical({
+            menu,
+            main_content_components[0], // Toate elementele trb sa fie in container
+            main_content_components[1], //altfel crapa
+            main_content_components[2],
+            bar_content_components[0],
+            bar_content_components[1],
+        });
+
+        root_component = Renderer(content_container, [&] {
+            Component current_main = GetCurrentMain();
+            Component current_bar = GetCurrentBarComponent();
+
+            auto top_section = hbox({
+            menu->Render() | border |size(WIDTH, EQUAL, 30),
+            current_main->Render() | flex,
+            }) | flex | size(HEIGHT, EQUAL, 40);
+
+            auto bottom_section = vbox({
+            current_bar->Render() | flex,
+            }) | flex_grow | vscroll_indicator | frame;
+
+            return vbox({
+                top_section,
+                bottom_section,
+            }) | border;
+        });
+
+    }
+
+
 
     void Run() {
-         BorderRenderer renderer(screen);
-        auto ui = Renderer([&] {return renderer.Render(); });
-        screen.Loop(ui);
+        screen.Loop(root_component);
+        current_config.save();
     }
 };
+
+
+
 
 
 int main() {
-    BorderScreen app;
+    auto screen = ScreenInteractive::Fullscreen();
+    //std::string filename = GetConfigFileLocation(); LATER IMPLEMMENTED
+    //LoadedCfg = Config(filename);
+
+    Config loaded_cfg;
+
+    App app(screen, loaded_cfg);
     app.Run();
 
     return 0;
